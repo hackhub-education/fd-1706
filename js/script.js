6010e4a27d8349cf54e7bcd0ccd37bb7722df22e
Yan Hong <onlyhongyan@gmail.com>
2017-06-04 12:12:39 -0700

    add click event and get studnet detail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
-$.get('http://192.168.1.124:3000/student', function(response) {
+var host = 'http://192.168.1.124:3000/'
 
-	console.log(response)
+$.get(host + 'student', function(response) {
+	// for (var i = 0; i < response.length; i++) {
+	// 	$('<a>')
+	// 	.text(response[i].name)
+	// 	.attr('href', 'http://192.168.1.124:3000/student/' + response[i]._id)
+	// 	.appendTo('body')
+	// }
+
+	response.forEach(function(student) {
+		$('<a>')
+			.text(student.name)
+			.addClass('get-student-info')
+			.attr('id', student._id)
+			// .attr('href', 'http://192.168.1.124:3000/student/' + student._id)
+			.appendTo('body')
+	})
+
+	// $('.get-student-info').click(function() {
+	// 	console.log('Clicked!')
+	// })
 
 })
+
+
+$('body').on('click', '.get-student-info', function(e) {
+	$.get(host + 'student/' + e.target.id, function(response) {
+		console.log(response);
+	})
+
+})
+
+
