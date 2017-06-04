fab9f9304a304b96795bab89a0de65a4aaaaa33b
Yan Hong <onlyhongyan@gmail.com>
2017-06-04 15:53:27 -0700

    update student info

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $.get(host + 'student', function(response) {
 
 	response.forEach(function(student) {
 		var listItem = $('<a>')
-			.text(student.name)
+			.html('<p>' + student.name + '</p>')
 			.addClass('get-student-info')
 			.attr('id', student._id)
 			// .attr('href', 'http://192.168.1.124:3000/student/' + student._id)
@@ -34,6 +34,7 @@ $('body').on('click', '.get-student-info', function(e) {
 		$('.student-name').text(response.name || "");
 		$('.student-age').text(response.age || "");
 		$('.student-school').text(response.school || "");
+		$('.update-info').attr('data-id', e.target.id);
 
 	})
 
@@ -52,6 +53,49 @@ $('body').on('click', '.remove-btn', function(e) {
 	});
 
 })
+
+$('body').on('click', '.update-info', function(e) {
+	
+	$('.update-form').show();
+	$('.update-info').hide();
+
+	$('#update-name').val($('.student-name').text() || "");
+	$('#update-age').val($('.student-age').text() || "");
+	$('#update-school').val($('.student-school').text() || "");
+	$('#update-id').val($('.update-info').attr('data-id') || "");
+})
+
+
+$('body').on('click', '.submit-update', function(e) {
+
+	e.preventDefault();
+
+	var student = {
+		"_id": $('#update-id').val(),
+		"name": $('#update-name').val(),
+		"age": $('#update-age').val(),
+		"school": $('#update-school').val()
+	}
+
+
+	$.ajax({
+	  type: "POST",
+	  url: host+'update',
+	  data: JSON.stringify(student),
+	  success: function(response) {
+	  	$('.student-name').text(student.name || "");
+		$('.student-age').text(student.age || "");
+		$('.student-school').text(student.school || "");
+		$('#' + student._id).find('p').text(student.name)
+	  },
+	  dataType: 'json',
+	  contentType: 'application/json',
+	});
+
+})
+
+
+
 
 $('#submit-add').click(function(e) {
 	e.preventDefault();
